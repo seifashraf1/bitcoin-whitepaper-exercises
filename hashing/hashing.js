cb31dc887e9c562363af76f73062d81e29699ff7
seifashraf1 <seifsefo50@gmail.com>
2021-10-03 19:32:54 +0200

    some bugs related to checking block data, try next time to remove a line from the poem and see block verfication

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -9,12 +9,12 @@ var poem = [
 	"and the power of fire can burn",
 	"the power of wind can chill",
 	"and the power of a mind can learn",
-	"the power of anger can rage",
+	"",
 	"inside until it tears u apart",
 	"but the power of a smile",
 	"especially yours can heal a frozen heart",
 ];
-
+//the power of anger can rage
 //index var 
 let indx = 0;
 
@@ -26,53 +26,78 @@ var Blockchain = {
 Blockchain.blocks.push({
 	index: 0,
 	hash: "000000",
-	data: "",
+	data: "somedata",		//here I put some data in the genesis block cuz I verify the block by checking on non-empty data
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-
 for (let line of poem) {
-	let block = createBlock(line)
-
-	Blockchain.blocks.push({
-		index: block.index,
-		hash: block.hash,
-		data: block.data,
-		timestamp: block.timestamp,
-		prevHash: block.prevHash
-	});
+	createBlock(line);
 }
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
 
-function createBlock(text) {
-	indx++;
+function createBlock(data) {
 
 	let block = {
-		index: indx,
-		prevHash: Blockchain.blocks[indx-1].hash,
-		data: text,
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
+		data: data,
 		timestamp: Date.now(),
 	}
 
-	
-	block.hash = blockHash(block)
 
-	return block
+	block.hash = blockHash(block);
+
+	Blockchain.blocks.push(block);
+
+	return block;
 }
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
-		bl.index,
-		bl.prevHash,
-		bl.data,
-		bl.timestamp
+		
+		bl.index+bl.prevHash+bl.data+bl.timestamp
 
 	).digest("hex");
+}
+
+function verifyBlock(block) {
+	if (block.index == 0) {		//check if genesis block 
+		if (block.data.length > 0 && block.hash === "000000") return true;
+		else return false;
+
+	} else {
+		
+		if (block.index < 0) return false;
+		
+		else {
+			if (block.data.length > 0 && block.prevHash && block.hash === blockHash(block)) return true;
+			else return false;
+		}
+	}
+}
+function verifyChain (blockchain) {
+
+	let blocks = blockchain.blocks;
+
+	for (let block of blocks) {
+		if (!verifyBlock(block)) return false;
+		
+		if (block.index > 0) {
+			let prev_block = blocks[block.index-1];
+			let prev_block_hash = blockHash(prev_block);
+
+			if (block.prevHash === prev_block_hash) return true;
+			else return false;
+		} else {
+			return true;
+		}
+
+	}
+		
 }
